4d9ee2a6023fd0508e154e83055688b422b74265
Ejims Chigbogu <stanleychigbogu@gmail.com>
2025-05-21 15:58:31 +0100

    pdate 21 may added some validations before udating user details back into the csv

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # simple atm machine
 little projects to help improve learning
 
-Updates 
+Updates 20 may
 Saved the Users to a csv file 
 will be reading and updating user accounts from csv files instead of static dict
 
+update 21 may 
+added some validations before udating user details back into the csv 
--- atm.py
+++ atm.py
@@ -1,6 +1,7 @@
 """
 ATM machine
 """
+
 import csv
 
 
@@ -12,23 +13,24 @@ def auth_user():
             # prompt for pin and then check the user Db to see if there a match
             user_pin = str(input("\n>>> Enter 4 digits PIN to continue: "))
 
-            #open CSV, read through and check if the user_pin is in PIN
+            # open CSV, read through and check if the user_pin is in PIN
             try:
                 with open("users.csv", "r") as file:
                     reader = csv.DictReader(file)
                     for row in reader:
-                        if user_pin  == row['PIN']:
+                        if user_pin == row["PIN"]:
                             auth_complete = True
                             break
 
-                    #Wrong user PIN
-                    if user_pin not in row["PIN"]:
+                    # Wrong user PIN
+                    if not auth_complete:
                         print("\n❌ Wrong PIN, try again\n")
+
             except FileNotFoundError as e:
                 file_name = e.filename
                 print(f" ⚠️ The file '{file_name}' was not found.")
         except ValueError:
-            print("\n⚠️ Enter the correct 4 digit PIN to continue\n")        
+            print("\n⚠️ Enter the correct 4 digit PIN to continue\n")
     return user_pin
 
 
@@ -38,76 +40,125 @@ def check_balance(user_pin):
             reader = csv.DictReader(file)
             for row in reader:
                 if row["PIN"] == user_pin:
+                    print(f"\n😊 Welcome back, {row['Name']}\n")
                     print(f"\n✅ Account balance: ${row['Balance']}\n")
     except FileNotFoundError as e:
         file_name = e.filename
-        print(f" ⚠️ The file '{file_name}' was not found.")
-    
+        print(f" ⚠️ {file_name} was not found.")
 
 
 def deposit(user_pin):
-    pass
-    # while True:
-    #     try:
-    #         amount_deposite = float(input(">>> Enter amount to deposit: "))
-    #         copy_amount_deposite = amount_deposite
+    deposit_successful = False
+    while not deposit_successful:
+        # open users.csv Db in read mode
+        try:
+            all_users_list = []
+            with open("users.csv", "r") as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    all_users_list.append(row)
 
-    #         if copy_amount_deposite:
-    #             copy_amount_deposite += users[user_pin]["balance"]
-    #             print(f'\n✅ The sum ${amount_deposite} has been deposited to your account successfully!!')
-    #             print('✅ Deposite successfull!\n')
-    #             break
-    #         else:
-    #             print('⚠️ Enter the exact amount to deposite')
-    #     except ValueError:
-    #         print('⚠️ Enter Number value')
-    #         continue
+                #loop through the list containing dictionaries of users "all_user_list""
+                for user in all_users_list:
+                    if user["PIN"] == user_pin:
+                        print(f"\n😊 Welcome back, {user['Name']}\n")
+                        #promt for amount since user exists
+                        try:
+                            deposit_amount = float(input(">>> Enter amount to deposit: "))
+                        except ValueError:
+                            print("⚠️ Enter dollar value")
 
-    #     #Display Option to view Balance after withdrawal    
-    #     try:
-    #         option_check_balance = input(">>> Press (Y/N) to see account balance: ").title()
-    #         if option_check_balance == "Y":
-    #             check_balance(user_pin)  # just printing the bal after deposit to be sure
-    #     except ValueError:
-    #         print('⚠️ Use Y or N')
+                        # convert the user's balance to float
+                        balanceTo_float = float(user["Balance"])
+
+                        # validate deposite amount  > 0
+                        if deposit_amount > 0:
+                            balanceTo_float += deposit_amount
+                            user["Balance"] = balanceTo_float
+                            deposit_successful = True
+                            break
+                        else:
+                            print("⚠️ Enter positive amount")
+
+            # update users Db only after successful deposit
+            if deposit_successful:
+                try:
+                    with open("users.csv", "w") as file:
+                        writer = csv.DictWriter(file, fieldnames=["PIN", "Name", "Balance"])
+                        writer.writeheader()
+                        writer.writerows(all_users_list)
+                except FileNotFoundError as e:
+                    file_name = e.filename
+                    print(f"⚠️ {file_name} not found.")
+
+                # print success message to the screen
+                print(f"\n✅ The sum ${deposit_amount} has been deposited to your account successfully!!")
+
+        except FileNotFoundError as e:
+            file_name = e.filename
+            print(f"⚠️ {file_name} not found.")
+
 
 def withdraw(user_pin):
-    pass
-    # while True:
-        
-    #     try:
-    #         amount_withdraw = float(input(">>> Enter amount to withdraw: "))
-    #         copy_amount_withdraw = amount_withdraw
-            
-    #         if copy_amount_withdraw > users[user_pin]["balance"]:
-    #             print("\n⚠️ Insufficient balance.")
-    #             insufficient_count = 0
-    #             while insufficient_count < 2:
-    #                 print(insufficient_count)
-    #                 insufficient_count += 1
-    #                 print(insufficient_count)
-
+    withdrawal_successful = False
+    while not withdrawal_successful:
+        try:
+            all_user_list = [] 
+            with open("users.csv", "r") as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    #add all user dictionary from csv file to a list
+                    all_user_list.append(row)
                     
-    #         # print(f"FYI: Your balance ${users[user_pin]['balance']}.")
-    #         elif copy_amount_withdraw <= 0:
-    #             print("\n⚠️ Minimum withdrawal amount $5")
-    #         elif copy_amount_withdraw == users[user_pin]["balance"]:
-    #             #recomfirn if user wants to do max withdrawal, if yes go ahead
-    #             try:
-    #                 two_factor = str(input('''\n⚠️ You about to withdraw the max amount in this account, Use "Y" to continue with max withdrawal "N" to discontinue: ''')).title()
-    #                 if two_factor == "Y":
-    #                     users[user_pin]["balance"] -= copy_amount_withdraw
-    #                     print("\n✅ Withdrawal Successfully!!\n")
-    #                     break
-    #             except ValueError:
-    #                 print('\n⚠️ Use "Y" to continue OR "N" to disconue.\n')
-    #         else:
-    #             users[user_pin]["balance"] -= copy_amount_withdraw
-    #             print("\n✅ Withdrawal Successfully!!\n")
-    #             break
-                
-    #     except ValueError:
-    #         print("\n⚠️ Enter amout to withdraw\n")
+                # loop through the list containing dictionaries of users "all_user_list""
+                for user in all_user_list:
+                    if user["PIN"] == user_pin:
+                        print(f"\n😊 Welcome back, {user['Name']}\n")
+                        #prompt for amount
+                        try:
+                            amount_withdraw = float(input(">>> Enter amount to withdraw: "))
+                        except ValueError:
+                            print("⚠️Enter positive amount")
+
+                        #convert user's balance to float for float to float conparison
+                        balanceTo_float = float(user["Balance"])
+
+                        if amount_withdraw > balanceTo_float:
+                            print("⚠️ Insufficient balance")
+                        elif amount_withdraw <= 0:
+                            print("⚠️ Minimum withdrawal is $1")
+                        elif amount_withdraw == balanceTo_float:
+                            #recomfirn if user wants to do max withdrawal, if yes go ahead
+                            try:
+                                confirm_max_withdrawal = str(input('''\n⚠️ You about to withdraw the max amount in this account, Use "Y" to continue with max withdrawal "N" to discontinue: ''')).title()
+                                if confirm_max_withdrawal == "Y":
+                                    balanceTo_float -= amount_withdraw
+                                    print("\n✅ Withdrawal Successfully!!\n")
+                                    user["Balance"] = balanceTo_float
+                                    withdrawal_successful = True
+                                    break
+                            except ValueError:
+                                print('\n⚠️ Use "Y" to continue OR "N" to disconue.\n')
+                        else:
+                            balanceTo_float -= amount_withdraw
+                            print("\n✅ Withdrawal Successfully!!\n")
+                            user["Balance"] = balanceTo_float
+                            withdrawal_successful = True
+                            break
+
+                #Update User including the new changes
+                if withdrawal_successful:
+                    try:
+                        with open("users.csv", "w") as file:
+                            writer = csv.DictWriter(file, fieldnames=["PIN","Name","Balance"])
+                            writer.writeheader()
+                            writer.writerows(all_user_list)
+                    except FileNotFoundError as e:
+                        file_name = e.filename
+                        print(f"⚠️ {file_name} not found")
+
+        except ValueError:
+            print("\n⚠️ Enter amout to withdraw\n")
 
 
 def exit():
@@ -117,18 +168,21 @@ def exit():
 def main():
     while True:
         # Display welcome message
-        print("\nGood day!!, Welcome to this ATM😊")
-        print('What would you like to do?, Choose from the menu codes below\n')
+        print("\n------------------------------------------------------------")
+        print("Good day!!, Welcome to this ATM😊")
+        print("What would you like to do?, Choose from the menu codes below\n")
         try:
-            main_menu = str(input(
-        """> Check Balance. (B)\n
+            main_menu = str(
+                input(
+                    """> Check Balance. (B)\n
 > Deposit Money. (D)\n
 > Make a Withdraw. (W)\n
-> Exit (E).\n>>>: """)
+> Exit (E).\n>>>: """
+                )
             ).title()
 
-            if main_menu in ["B","D","W"]:
-                user_pin = auth_user() #catch the returned auth_user
+            if main_menu in ["B", "D", "W"]:
+                user_pin = auth_user()  # catch the returned auth_user
                 if main_menu == "B":
                     check_balance(user_pin)
                 elif main_menu == "D":
